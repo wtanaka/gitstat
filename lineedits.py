cc51a90d6828db64233daaecf97b44f8d0d358b1
Wesley Tanaka <wtanaka@yahoo.com>
2014-07-13 14:42:51 -1000

    Fix pylint errors

--- lineedits.py
+++ lineedits.py
@@ -16,6 +16,9 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with gitstat.  If not, see <http://www.gnu.org/licenses/>.
+"""Process the output of gitlogstat to produce a graph, by author,
+of touched lines of codes (additions + deletions)
+"""
 import calendar
 import datetime
 import fileinput
@@ -23,7 +26,6 @@ import itertools
 import json
 import logging
 import os.path
-import string
 import sys
 
 ADDS = 'adds'
@@ -34,27 +36,29 @@ TIME = 'datetime'
 ZERO = datetime.timedelta(0)
 class UTC(datetime.tzinfo):
     """UTC"""
-    def utcoffset(self, dt):
+    def utcoffset(self, _dtime):
         return ZERO
 
-    def tzname(self, dt):
+    def tzname(self, _dtime):
         return "UTC"
 
-    def dst(self, dt):
+    def dst(self, _dtime):
         return ZERO
 
 MY_UTC = UTC()
 
 def render_template(variables):
-    fp = open(os.path.join(os.path.dirname(__file__),
+    """Return a HTML string rendered with the given variables"""
+    file_handle = open(os.path.join(os.path.dirname(__file__),
             'lineedits.html.template'))
-    template = string.Template(fp.read())
-    return template.substitute(variables)
+    return file_handle.read() % variables
 
 def key_email_date(dictionary):
+    """Returns (EMAIL, TIME)"""
     return (dictionary[EMAIL], dictionary[TIME])
 
 def key_email(dictionary):
+    """Returns the EMAIL key"""
     return dictionary[EMAIL]
 
 def processed_lines(lines):
@@ -64,11 +68,11 @@ def processed_lines(lines):
     commit = None
     for line in lines:
         line = line.strip()
-        if line.startswith("XXX"):
+        if line.startswith("COMMIT"):
             if (commit is not None and
                     (commit[ADDS] + commit[DELETES] > 0)):
                 yield commit
-            email, name, timestamp = line[3:].split('|')
+            email, name, timestamp = line[6:].split('|')
             timestamp = long(timestamp)
 
             commit = {
@@ -88,18 +92,18 @@ def processed_lines(lines):
                 else:
                     commit[ADDS] += int(adds)
                     commit[DELETES] += int(removes)
-            except ValueError, e:
+            except ValueError:
                 logging.error("Could not parse '%s'", line)
                 raise
 
-def sort_by_author_date(processed_lines):
+def sort_by_author_date(parsed_lines):
     """Takes a iterator of dicts produced by processed_lines() and
     returns a dict
     """
-    return sorted(processed_lines, key=key_email_date)
+    return sorted(parsed_lines, key=key_email_date)
 
 def group_by_author(sorted_lines):
-    """
+    """Group the lines by author
     """
     return itertools.groupby(sorted_lines, key=key_email)
 
@@ -112,42 +116,50 @@ def removecommitsbefore(commitwindow, cutoff):
         commitwindow = commitwindow[1:]
     return commitwindow
 
+def make_timeseries(grouped_lines, window_size):
+    """Convert the output of group_by_author into timeseries data
+    suitable for flot
+    """
+    timeseries = {}
+    for email, commits in grouped_lines:
+        timeseries[email] = []
+        # List of commits 
+        commitwindow = []
+        for commit in commits:
+            timetuple = commit[TIME].utctimetuple()
+            commitwindow.append(commit)
+            commitwindow = removecommitsbefore(commitwindow,
+                  commit[TIME] - window_size)
+            linestouched = sum(tuple(c[ADDS] + c[DELETES] for c in
+                     commitwindow))
+            pair = (1000 * calendar.timegm(timetuple), linestouched)
+            timeseries[email].append(pair)
+    return timeseries
+
 def main():
-   logging.basicConfig()
-   parsed_lines = processed_lines(fileinput.input())
-   sorted_lines = sort_by_author_date(parsed_lines)
-   del parsed_lines
-   grouped_lines = group_by_author(sorted_lines)
-   del sorted_lines
+    """Main function"""
+    logging.basicConfig()
+    parsed_lines = processed_lines(fileinput.input())
+    sorted_lines = sort_by_author_date(parsed_lines)
+    del parsed_lines
+    grouped_lines = group_by_author(sorted_lines)
+    del sorted_lines
 
-   window_size = datetime.timedelta(days=30)
+    window_size = datetime.timedelta(days=30)
 
-   timeseries = {}
-   for email, commits in grouped_lines:
-       timeseries[email] = []
-       # List of commits 
-       commitwindow = []
-       for commit in commits:
-           timetuple = commit[TIME].utctimetuple()
-           commitwindow.append(commit)
-           commitwindow = removecommitsbefore(commitwindow,
-                 commit[TIME] - window_size)
-           linestouched = sum(tuple(c[ADDS] + c[DELETES] for c in
-                    commitwindow))
-           pair = (1000 * calendar.timegm(timetuple), linestouched)
-           timeseries[email].append(pair)
+    timeseries = make_timeseries(grouped_lines, window_size)
 
-   plotdata = [{
-      'data': series,
-      'label': label,
-      'points': {'show': True},
-      'lines': {'show': True, 'fill': True},
-      'color': idx,
-   } for idx, (label, series) in enumerate(timeseries.iteritems())]
+    plotdata = [{
+       'data': series,
+       'label': label,
+       'points': {'show': True},
+       'lines': {'show': True, 'fill': True},
+       'color': idx,
+    } for idx, (label, series) in enumerate(timeseries.iteritems())]
 
-   variables = {}
-   variables['plotdata'] = json.dumps(plotdata)
-   sys.stdout.write(render_template(variables))
+    variables = {}
+    variables['plotdata'] = json.dumps(plotdata)
+    sys.stdout.write(render_template(variables))
 
 if __name__ == "__main__":
     main()
